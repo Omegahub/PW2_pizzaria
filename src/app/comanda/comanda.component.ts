ee52b110147f71099734601ee8bc453ddd823528
Omegahub <Omegahub2019@gmail.com>
2019-11-04 22:15:43 -0200

    atividade 8

--- src/app/comanda/comanda.component.ts
+++ src/app/comanda/comanda.component.ts
@@ -18,8 +18,34 @@ export class ComandaComponent implements OnInit {
       Pizza : this.formBuilder.control(''),
       Recheio : this.formBuilder.control(''),
       Pessoas : this.formBuilder.control(''),
-      TaxaServico : this.formBuilder.control(''),
+      TaxaServico : this.formBuilder.control(false)
     })
   }
 
+  onProcessar(){
+    let vlChopp:number = this.formcomanda.value.Chopp * 7.30;
+    let vlPizza:number = this.formcomanda.value.Pizza * 31.50;
+    let vlRecheio:number = this.formcomanda.value.Recheio * 5.90;
+    let vlPessoas:number = this.formcomanda.value.Pessoas;
+    let vlTotal: number =  (vlChopp + vlPizza + vlRecheio) / vlPessoas;
+    let vlConsumo: number =  (vlChopp + vlPizza + vlRecheio);
+    let TaxaServico:boolean = this.formcomanda.value.TaxaServico;
+    let vlTaxaServico: number = (vlChopp + vlPizza + vlRecheio) / vlPessoas * 0.1;
+    let vlTotalTaxa: number =  (vlChopp + vlPizza + vlRecheio + vlTaxaServico) / vlPessoas;
+
+    if(TaxaServico == true)
+    {
+      alert("Consumo : " + vlConsumo + 
+      " \n Serviços : " + vlTaxaServico + 
+       "\n Total " + vlTotalTaxa +
+       "\n Por pessoa:" + vlTotal   );
+    }
+    else
+    {
+      alert("Valor total : " + vlTotal);
+    }
+
+
+
+  }
 }
